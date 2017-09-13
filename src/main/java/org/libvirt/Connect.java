0a4f7c910c5796eb7ea6021681e9ca88b3f1bb55
Boris Schrijver <bschrijver@schubergphilis.com>
2017-09-13 16:02:32 +0200

    Extract DomainEventIDs to the enums package

--- src/main/java/org/libvirt/Connect.java
+++ src/main/java/org/libvirt/Connect.java
@@ -1,10 +1,10 @@
 package org.libvirt;
 
-import static org.libvirt.BitFlagsHelper.OR;
 import static org.libvirt.ErrorHandler.processError;
 import static org.libvirt.ErrorHandler.processErrorIfZero;
 import static org.libvirt.Library.getConstant;
 import static org.libvirt.Library.libvirt;
+import static org.libvirt.flags.BitFlagsHelper.OR;
 
 import java.net.URI;
 import java.util.HashMap;
@@ -29,6 +29,7 @@ import org.libvirt.event.PMSuspendReason;
 import org.libvirt.event.PMWakeupListener;
 import org.libvirt.event.PMWakeupReason;
 import org.libvirt.event.RebootListener;
+import org.libvirt.event.enums.DomainEventID;
 import org.libvirt.jna.Libvirt;
 import org.libvirt.jna.callbacks.VirConnectCloseFunc;
 import org.libvirt.jna.callbacks.VirConnectDomainEventBlockJobCallback;
@@ -97,122 +98,6 @@ public class Connect {
     }
 
     private CloseFunc registeredCloseFunc = null;
-
-    /**
-     * Event IDs.
-     */
-    private enum DomainEventID {
-        /**
-         * virConnectDomainEventCallback
-         */
-        VIR_DOMAIN_EVENT_ID_LIFECYCLE(0),
-        /**
-         * virConnectDomainEventGenericCallback
-         */
-        VIR_DOMAIN_EVENT_ID_REBOOT(1),
-        /**
-         * virConnectDomainEventRTCChangeCallback
-         */
-        VIR_DOMAIN_EVENT_ID_RTC_CHANGE(2),
-        /**
-         * virConnectDomainEventWatchdogCallback
-         */
-        VIR_DOMAIN_EVENT_ID_WATCHDOG(3),
-        /**
-         * virConnectDomainEventIOErrorCallback
-         */
-        VIR_DOMAIN_EVENT_ID_IO_ERROR(4),
-        /**
-         * virConnectDomainEventGraphicsCallback
-         */
-        VIR_DOMAIN_EVENT_ID_GRAPHICS(5),
-        /**
-         * virConnectDomainEventIOErrorReasonCallback
-         */
-        VIR_DOMAIN_EVENT_ID_IO_ERROR_REASON(6),
-        /**
-         * virConnectDomainEventGenericCallback
-         */
-        VIR_DOMAIN_EVENT_ID_CONTROL_ERROR(7),
-        /**
-         * virConnectDomainEventBlockJobCallback
-         */
-        VIR_DOMAIN_EVENT_ID_BLOCK_JOB(8),
-        /**
-         * virConnectDomainEventDiskChangeCallback
-         */
-        VIR_DOMAIN_EVENT_ID_DISK_CHANGE(9),
-        /**
-         * virConnectDomainEventTrayChangeCallback
-         */
-        VIR_DOMAIN_EVENT_ID_TRAY_CHANGE(10),
-        /**
-         * virConnectDomainEventPMWakeupCallback
-         */
-        VIR_DOMAIN_EVENT_ID_PMWAKEUP(11),
-        /**
-         * virConnectDomainEventPMSuspendCallback
-         */
-        VIR_DOMAIN_EVENT_ID_PMSUSPEND(12),
-        /**
-         * virConnectDomainEventBalloonChangeCallback
-         */
-        VIR_DOMAIN_EVENT_ID_BALLOON_CHANGE(13),
-        /**
-         * virConnectDomainEventPMSuspendDiskCallback
-         */
-        VIR_DOMAIN_EVENT_ID_PMSUSPEND_DISK(14),
-        /**
-         * virConnectDomainEventDeviceRemovedCallback
-         */
-        VIR_DOMAIN_EVENT_ID_DEVICE_REMOVED(15),
-        /**
-         * virConnectDomainEventBlockJobCallback
-         */
-        VIR_DOMAIN_EVENT_ID_BLOCK_JOB_2(16),
-        /**
-         * virConnectDomainEventTunableCallback
-         */
-        VIR_DOMAIN_EVENT_ID_TUNABLE(17),
-        /**
-         * virConnectDomainEventAgentLifecycleCallback
-         */
-        VIR_DOMAIN_EVENT_ID_AGENT_LIFECYCLE(18),
-        /**
-         * virConnectDomainEventDeviceAddedCallback
-         */
-        VIR_DOMAIN_EVENT_ID_DEVICE_ADDED(19),
-        /**
-         * virConnectDomainEventMigrationIterationCallback
-         */
-        VIR_DOMAIN_EVENT_ID_MIGRATION_ITERATION(20),
-        /**
-         * virConnectDomainEventJobCompletedCallback
-         */
-        VIR_DOMAIN_EVENT_ID_JOB_COMPLETED(21),
-        /**
-         * virConnectDomainEventDeviceRemovalFailedCallback
-         */
-        VIR_DOMAIN_EVENT_ID_DEVICE_REMOVAL_FAILED(22),
-        /**
-         * virConnectDomainEventMetadataChangeCallback
-         */
-        VIR_DOMAIN_EVENT_ID_METADATA_CHANGE(23),
-        /**
-         * virConnectDomainEventBlockThresholdCallback
-         */
-        VIR_DOMAIN_EVENT_ID_BLOCK_THRESHOLD(24);
-
-        private int n;
-
-        DomainEventID(int n) {
-            this.n = n;
-        }
-
-        public int getValue() {
-            return n;
-        }
-    }
 
     public enum OpenFlags implements BitFlags {
         /**
