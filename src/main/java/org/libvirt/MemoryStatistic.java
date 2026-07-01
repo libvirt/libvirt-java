51bb8048913187d088e908c72a0199e48c4f6b38
Takashi Kajinami <kajinamit@oss.nttdata.com>
2026-07-01 13:19:46 +0900

    Introduce class to reflect virDomainMemoryStatTags
    Signed-off-by: Takashi Kajinami <kajinamit@oss.nttdata.com>

--- src/main/java/org/libvirt/MemoryStatistic.java
+++ src/main/java/org/libvirt/MemoryStatistic.java
@@ -7,6 +7,36 @@ public class MemoryStatistic {
     protected int tag;
     protected long val;
 
+    public static final class MemoryStatisticTags {
+        public static final int SWAP_IN        = 0;
+
+        public static final int SWAP_OUT       = 1;
+
+        public static final int MAJOR_FAULT    = 2;
+
+        public static final int MINOR_FAULT    = 3;
+
+        public static final int UNUSED         = 4;
+
+        public static final int AVAILABLE      = 5;
+
+        public static final int ACTUAL_BALLOON = 6;
+
+        public static final int RSS            = 7;
+
+        public static final int USABLE         = 8;
+
+        public static final int LAST_UPDATE    = 9;
+
+        public static final int DISK_CACHES    = 10;
+
+        public static final int PGALLOC        = 11;
+
+        public static final int HUGETLB_PGFAIL = 12;
+
+        public static final int NR             = 13;
+    }
+
     public MemoryStatistic(final virDomainMemoryStats stat) {
         tag = stat.tag;
         val = stat.val;
